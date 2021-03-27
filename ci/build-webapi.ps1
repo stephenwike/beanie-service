b74cae94083b3d4fe6fac7982d4546d8ae6ad00a
Stephen Wike <stephen.c.wise@gmail.com>
2021-03-27 10:14:55 -0600

    fixed push location

--- ci/build-webapi.ps1
+++ ci/build-webapi.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 
-Push-Location ./src/beanie-service
+Push-Location ./src/beanie-webapi
    dotnet publish -c $env:BUILD_CONFIGURATION -f netcoreapp3.1  -r linux-x64 -o out
    docker build -t "$env:DOCKER_REPO/${env:SERVICE_DOCKER_IMAGE}:$env:VERSION" -t "$env:DOCKER_REPO/${env:SERVICE_DOCKER_IMAGE}:latest" .
 Pop-Location
