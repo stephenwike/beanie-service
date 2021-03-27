a17c4905d04af080cdb14377316cdd4065e450ce
Stephen Wike <stephen.c.wise@gmail.com>
2021-03-26 19:44:23 -0600

    added kubeconfig

--- ci/deploy-helm-chart.ps1
+++ ci/deploy-helm-chart.ps1
@@ -1,6 +1,18 @@
 #!/usr/bin/env pwsh
 
-helm uninstall $env:HELM_CHART_NAME
+# Build kubeconfig
+kubectl config set-cluster $env:KUBERNETES_CLUSTER_NAME `
+    --server=$env:KUBERNETES_SERVER
+kubectl config set clusters.$env:KUBERNETES_CLUSTER_NAME.certificate-authority-data $env:KUBERNETES_CERTIFICATE
+kubectl config set-credentials $env:KUBERNETES_CLUSTER_NAME
+kubectl config set users.$env:KUBERNETES_CLUSTER_NAME.client-certificate-data $env:KUBERNETES_CLIENT_CERTIFICATE
+kubectl config set users.$env:KUBERNETES_CLUSTER_NAME.client-key-data $env:KUBERNETES_CLIENT_KEY
+kubectl config set-context $env:KUBERNETES_CLUSTER_NAME `
+    --cluster=$env:KUBERNETES_CLUSTER_NAME `
+    --user=$env:KUBERNETES_CLUSTER_NAME
+kubectl config use-context $env:KUBERNETES_CLUSTER_NAME
 
+# Reinstall helm
+helm uninstall $env:HELM_CHART_NAME
 helm install $env:HELM_CHART_NAME ./chart/ `
     --set persistence.password=$env:POSTGRES_PASSWORD
